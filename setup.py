2fe23f6d5dbb3b0b83cc0b82b0a073790ced4799
LuCkEr <lucker@lucker.xyz>
2022-05-16 21:11:43 +0300

    Better requirements

--- setup.py
+++ setup.py
@@ -17,6 +17,15 @@ requirements = []
 with open("requirements.txt", "r", encoding="utf-8") as f:
     requirements += f.read().splitlines()
 
+extras_require = {
+    'test': [
+        'coverage',
+        'pytest',
+        'pytest-asyncio',
+        'pytest-cov'
+    ]
+}
+
 setuptools.setup(
     name="PyScoreSaber",
     version=__version__,
@@ -30,12 +39,22 @@ setuptools.setup(
         "Bug Tracker": "https://github.com/Kiyomi-Parents/PyScoreSaber/issues",
     },
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
+        "Typing :: Typed",
     ],
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.8",
-    install_requires=requirements
+    install_requires=requirements,
+    extras_require=extras_require,
 )
